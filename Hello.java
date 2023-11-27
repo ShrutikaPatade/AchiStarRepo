79313c73193c7620432c46a2c7e8e919f09d2ae1
Shrutika <Shrutika@AchiStar.com>
2023-11-27 17:00:06 +0000

    Checking automation

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
       System.out.println("This is a java app created using Docker");
+      System.out.println("This line is to check if automation is working fine");
      }
   } 
